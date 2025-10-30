3a7cb1b674e45d4837e6adb7516411e40378a6a3
V4ni11a <feewillowherb@outlook.com>
2025-10-30 10:45:03 +0800

    1

---
+++ MaterialClient.Common.UnitTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace MaterialClient.Common.UnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ MaterialClient.Common/EFCore/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaterialClient.EFCore;
+
+public class MyDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string encryptionPassword = "Your_New_Strong_Key";
+
+        string connectionString = new SqliteConnectionStringBuilder
+        {
+            DataSource = "my_official_encrypted_data.db",
+            Password = encryptionPassword
+        }.ToString();
+
+        optionsBuilder.UseSqlite(connectionString);
+    }
+}
