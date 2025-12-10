b273afdda2fd1eab47cd30c9549c71ef54e8d052
t321c <feewillowherb@outlook.com>
2025-12-10 17:30:54 +0800

    1

--- MaterialClient.Common/Api/IMaterialPlatformApi.cs
+++ MaterialClient.Common/Api/IMaterialPlatformApi.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel;
 using MaterialClient.Common.Api.Dtos;
 using Refit;
 
@@ -35,7 +36,39 @@ public interface IMaterialPlatformApi
     Task<HttpResult<object>> UserLoginAsync(
         [Body] LoginRequestDto request,
         CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// 授权机器码
+    /// </summary>
+    /// <param name="request">授权机器码请求</param>
+    /// <param name="cancellationToken">取消令牌</param>
+    /// <returns>登录用户信息</returns>
+    [Post("/api/ProjectMachine/AuthMachineCode")]
+    Task<HttpResult<bool>> AuthMachineCodeAsync(
+        [Body] GetAuthMachineCodeInput request,
+        CancellationToken cancellationToken = default);
+
+
+    /// <summary>
+    /// 验证项目/机器码
+    /// </summary>
+    /// <param name="request">验证项目/机器码请求</param>
+    /// <param name="cancellationToken">取消令牌</param>
+    /// <returns>登录用户信息</returns>
+    [Post("/api/ProjectMachine/AuthMachineCode")]
+    Task<HttpResult<bool>> VerificationMachineCodeAsync(
+        string proId,
+        string machineCode,
+        //OUT
+        VerificationMachineCodeEnum verificationMachineCode,
+        CancellationToken cancellationToken = default);
 }
+
+public record GetAuthMachineCodeInput(
+    string ProId,
+    string MachineCode,
+    string AuthToken
+);
 
 public record GetMaterialGoodListInput(
     string ProId,
@@ -51,3 +84,46 @@ public record GetMaterialProviderListInput(
     string ProId,
     long UploadTime
 );
+
+public enum VerificationMachineCodeEnum
+{
+    /// <summary>
+    /// 其他异常
+    /// </summary>
+    [Description("其他")] Other = -1,
+
+    /// <summary>
+    /// 未绑定(项目和机器码没有绑定)
+    /// </summary>
+    [Description("未绑定")] UnBounded = 0,
+
+    /// <summary>
+    /// 已绑定(项目和当前机器码一致，等同于相同电脑重新安装软件)
+    /// </summary>
+    [Description("项目和当前机器码一致")] BoundedWithThisErrorCode = 1,
+
+    /// <summary>
+    /// 已绑定(项目相同，机器码不一致)
+    /// </summary>
+    [Description("项目相同，机器码不一致")] BoundedWithErrorCode = 2,
+
+    /// <summary>
+    /// 已绑定（项目不同，机器码一致）
+    /// </summary>
+    [Description("项目不同，机器码一致")] BoundedWithManyErrorCode = 3,
+
+    /// <summary>
+    /// 授权Token验证失败
+    /// </summary>
+    [Description("授权Token验证失败")] InvalidAuthTokenErrorCode = 4,
+
+    /// <summary>
+    /// 授权有效期到期
+    /// </summary>
+    [Description("授权有效期到期")] AuthEndDateErrorCode = 5,
+
+    /// <summary>
+    /// 授权状态
+    /// </summary>
+    [Description("授权状态无效")] AuthStatusErrorCode = 6,
+}
