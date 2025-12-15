d3c584cea223f427dd717c20da5eda846db71309
V4ni11a <feewillowherb@outlook.com>
2025-12-15 17:41:38 +0800

    1

--- MaterialClient.Common/Migrations/20251215094020_Update_WeighingRecord_Materials.Designer.cs
+++ MaterialClient.Common/Migrations/20251215094020_Update_WeighingRecord_Materials.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using MaterialClient.EFCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
 #nullable disable
 
-namespace MaterialClient.Common.EntityFrameworkCore.Migrations
+namespace MaterialClient.Common.Migrations
 {
     [DbContext(typeof(MaterialClientDbContext))]
-    partial class MaterialClientDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251215094020_Update_WeighingRecord_Materials")]
+    partial class Update_WeighingRecord_Materials
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
