25e417dc2373cf6ffcd1e1f2618fa8240ed14c06
V4ni11a <feewillowherb@outlook.com>
2025-12-17 17:02:41 +0800

    1

---
+++ MaterialClient/Views/AttendedWeighing/WeighingRecordListView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MaterialClient.Views.AttendedWeighing;
+
+public partial class WeighingRecordListView : UserControl
+{
+    public WeighingRecordListView()
+    {
+        InitializeComponent();
+    }
+}
+
