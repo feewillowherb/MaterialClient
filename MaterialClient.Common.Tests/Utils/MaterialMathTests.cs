5cad7f6ba5f551068e63e37d911e79acf1e6bc0e
V4ni11a <feewillowherb@outlook.com>
2025-12-26 09:41:49 +0800

    1

---
+++ MaterialClient.Common.Tests/Utils/MaterialMathTests.cs
@@ -0,0 +1,42 @@
+﻿using MaterialClient.Common.Utils;
+using Shouldly;
+using Xunit;
+
+namespace MaterialClient.Common.Tests.Utils;
+
+public class MaterialMathTests
+{
+    [Fact]
+    public void DetermineOffsetResult_OverNegativeDeviation()
+    {
+        // Arrange
+        decimal? deviationRate = -5m; // -5%
+        decimal? lowerLimit = -3m;    // allowed lower limit -3%
+        decimal? upperLimit = null;
+
+        // Act
+        var result = MaterialMath.DetermineOffsetResult(deviationRate, lowerLimit, upperLimit);
+
+        // Assert
+        
+        result.ShouldBe(Entities.Enums.OffsetResultType.OverNegativeDeviation);
+    }
+
+
+    [Fact]
+    public void DetermineOffsetResult_OverPositiveDeviation()
+    {
+        // Arrange
+        decimal? deviationRate = 6m;  // 6%
+        decimal? lowerLimit = null;
+        decimal? upperLimit = 4m;     // allowed upper limit 4%
+
+        // Act
+        var result = MaterialMath.DetermineOffsetResult(deviationRate, lowerLimit, upperLimit);
+
+        // Assert
+        result.ShouldBe(Entities.Enums.OffsetResultType.OverPositiveDeviation);
+    }
+
+    
+}
