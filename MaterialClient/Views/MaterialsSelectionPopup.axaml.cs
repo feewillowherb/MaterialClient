8157e1ba9a7f2e05bfbd63142419aa6500895126
V4ni11a <feewillowherb@outlook.com>
2025-12-31 11:17:13 +0800

    1

---
+++ MaterialClient/Views/MaterialsSelectionPopup.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MaterialClient.Views;
+
+public partial class MaterialsSelectionPopup : UserControl
+{
+    public MaterialsSelectionPopup()
+    {
+        InitializeComponent();
+    }
+}
+
