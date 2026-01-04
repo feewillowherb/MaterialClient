f30d2350d4213e0afb7bff599de71d495eed008d
V4ni11a <feewillowherb@outlook.com>
2026-01-04 19:32:20 +0800

    1

--- MaterialClient/Views/AttendedWeighing/AttendedWeighingDetailView.axaml.cs
+++ MaterialClient/Views/AttendedWeighing/AttendedWeighingDetailView.axaml.cs
@@ -1,5 +1,6 @@
 using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using MaterialClient.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -31,5 +32,31 @@ public partial class AttendedWeighingDetailView : UserControl
 
     }
 
+    private void MaterialSelectionButton_Click(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button button && MaterialSelectionPopup != null && MaterialsSelectionPopupControl != null)
+        {
+            MaterialSelectionPopup.PlacementTarget = button;
+            
+            // 计算 HorizontalOffset 使 Popup 的左边缘对齐到 Button 的左边缘
+            // Placement="Bottom" 默认将 Popup 中心对齐到 Button 中心
+            // 要让左边缘对齐，需要向右偏移：(PopupWidth / 2) - (ButtonWidth / 2)
+            var popupWidth = MaterialsSelectionPopupControl.Width > 0 
+                ? MaterialsSelectionPopupControl.Width 
+                : 400; // MaterialsSelectionPopup 的默认宽度
+            
+            var buttonWidth = button.Bounds.Width > 0 
+                ? button.Bounds.Width 
+                : button.DesiredSize.Width;
+            
+            if (buttonWidth <= 0)
+            {
+                // 如果 Button 宽度还未测量，使用列宽 80
+                buttonWidth = 80;
+            }
+            
+            MaterialSelectionPopup.HorizontalOffset = (popupWidth / 2) - (buttonWidth / 2);
+        }
+    }
 
 }
