af75dafe8f8feae0c477131155a00496d86b7722
V4ni11a <feewillowherb@outlook.com>
2026-04-27 17:24:35 +0800

    Update MaterialUnitDisplayConverter.cs

--- MaterialClient/Converters/MaterialUnitDisplayConverter.cs
+++ MaterialClient/Converters/MaterialUnitDisplayConverter.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Globalization;
-using System.Reflection;
 using Avalonia.Data.Converters;
+using MaterialClient.Common.Entities;
 
 namespace MaterialClient.Converters;
 
@@ -13,63 +13,22 @@ public class MaterialUnitDisplayConverter : IValueConverter
 {
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value == null)
+        if (value is not Material material)
         {
             return "1个";
         }
 
-        var unitName = AsString(GetPropertyValue(value, "UnitName"));
-        var basicUnit = AsString(GetPropertyValue(value, "BasicUnit"));
-
-        if (string.IsNullOrWhiteSpace(unitName) || string.IsNullOrWhiteSpace(basicUnit))
+        if (string.IsNullOrWhiteSpace(material.UnitName) || string.IsNullOrWhiteSpace(material.BasicUnit))
         {
             return "1个";
         }
 
-        var unitRate = GetUnitRateValue(value) ?? 1m;
-        return $"1{unitName}={FormatRate(unitRate)}{basicUnit}";
+        return $"1{material.UnitName}={FormatRate(material.UnitRate)}{material.BasicUnit}";
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         throw new NotSupportedException();
-    }
-
-    private static object? GetPropertyValue(object? obj, string propertyName)
-    {
-        if (obj == null)
-        {
-            return null;
-        }
-
-        var prop = obj.GetType().GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Public);
-        return prop?.GetValue(obj);
-    }
-
-    private static string? AsString(object? value)
-    {
-        return value?.ToString();
-    }
-
-    private static decimal? GetUnitRateValue(object obj)
-    {
-        var rateObj = GetPropertyValue(obj, "UnitRate");
-        if (rateObj == null)
-        {
-            return null;
-        }
-
-        return rateObj switch
-        {
-            decimal d => d,
-            decimal? dn => dn,
-            double db => (decimal)db,
-            float f => (decimal)f,
-            int i => i,
-            long l => l,
-            string s when decimal.TryParse(s, out var parsed) => parsed,
-            _ => null
-        };
     }
 
     private static string FormatRate(decimal rate)
