efdeba6faadbed77d8c03b4700edcdc6ed115dff
V4ni11a <feewillowherb@outlook.com>
2026-07-02 09:25:21 +0800

    1

--- src/MaterialClient.Common/Utils/CodePagesEncodingInitializer.cs
+++ src/MaterialClient.Common/Utils/CodePagesEncodingInitializer.cs
@@ -3,7 +3,8 @@ using System.Text;
 namespace MaterialClient.Common.Utils;
 
 /// <summary>
-///     注册 CodePages 编码提供程序，使 GBK/GB2312 等代码页在 .NET Core 运行时可用。
+///     注册 CodePages 编码提供程序，使 GBK/GB2312 等代码页在 .NET 运行时可用。
+///     .NET 10 已由框架提供 <c>System.Text.Encoding.CodePages</c>，无需额外 NuGet 包；仅需调用 <see cref="Register"/>。
 /// </summary>
 public static class CodePagesEncodingInitializer
 {
@@ -11,7 +12,7 @@ public static class CodePagesEncodingInitializer
 
     /// <summary>
     ///     注册 <see cref="CodePagesEncodingProvider"/>（幂等，可多次调用）。
-    ///     应在应用启动最早阶段调用，且需引用 System.Text.Encoding.CodePages 包。
+    ///     应在应用启动最早阶段调用。
     /// </summary>
     public static void Register()
     {
