e01bcce9febfd8eb4d046a5de68646d944b26164
V4ni11a <feewillowherb@outlook.com>
2026-09-15 09:29:01 +0800

    Update publish-urban.cmd

--- scripts/publish-urban.cmd
+++ scripts/publish-urban.cmd
@@ -1,3 +1,1 @@
-@echo off
-cd /d "%~dp0.."
 dotnet publish src/MaterialClient.Urban/MaterialClient.Urban.csproj -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true
